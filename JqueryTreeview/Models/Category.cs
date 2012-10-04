50a0e5bdabe1e6a87afb435736ed5f1c94d9d286
Rodrigo Ipaves de Campos Pinto <ipaves@gmail.com>
2012-10-04 17:26:28 -0300

    adding the model to be the base of the info struct

---
+++ JqueryTreeview/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JqueryTreeview.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Category Upper { get; set; }
+    }
+}
