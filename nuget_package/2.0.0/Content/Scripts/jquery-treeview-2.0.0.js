4b5d0dc79c35af57b3582658e4cff62ff1a7a70e
Rodrigo Ipaves de Campos Pinto <ipaves@gmail.com>
2014-01-23 09:49:57 -0200

    just keeping alive

--- nuget_package/2.0.0/Content/Scripts/jquery-treeview-2.0.0.js
+++ nuget_package/2.0.0/Content/Scripts/jquery-treeview-2.0.0.js
@@ -6,37 +6,40 @@ var Treeview = (function () {
         this.options = options;
 
         this.renderItem = this.options.renderItem || this.renderItem;
-        this.getChildrenNodes = this.options.getChildrenNodes || this.getChildrenNodes;
-        this.getChildrenNodes(0, this.myObject, $.proxy(this.getChildrenNodesSuccess, this));
+
+        if (this.options.loadNodesOnDemand) {
+            this.getChildrenNodes = this.options.getChildrenNodes || this.getChildrenNodes;
+            this.getChildrenNodes(0, this.myObject, $.proxy(this.getChildrenNodesSuccess, this));
+        } else {
+            this.getAllNodes();
+        }
     }
 
     treeview.prototype = {
         getChildrenNodes: function (itemId, baseElement, funcCallback) {
-            var dataToSend = { upperId: itemId };
-            $.extend(dataToSend, this.options.dataToSend);
+            var baseDataToSend = { upperId: itemId };
+            var dataToSend = $.extend(true, baseDataToSend, this.options.dataToSend);
+            dataToSend = dataToSend == null ? "" : this.convertToString(dataToSend);
 
-            if (typeof this.options.sourceUrl === "string") {
-                $.ajax({
-                    type: this.options.ajaxVerb,
-                    contentType: "application/json; charset=utf-8",
-                    url: this.options.sourceUrl,
-                    data: this.convertToString(dataToSend),
-                    dataType: "json",
-                    success: function (data) {
-                        funcCallback(itemId, baseElement, data);
-                    },
-                    error: function (jqXhr, textStatus, errorThrown) {
-                        console.log("An error occurred when trying to obtain the nodes\n\njqXhr>" + jqXhr + "\n\ntextStatus>" + textStatus + "\n\nerrorThrown>" + errorThrown);
-                    }
-                });
-            }
-            ;
+            $.ajax({
+                type: this.options.ajaxVerb,
+                contentType: "application/json; charset=utf-8",
+                url: this.options.sourceUrl,
+                data: dataToSend,
+                dataType: "json",
+                success: function (data) {
+                    funcCallback(itemId, baseElement, data);
+                },
+                error: function (jqXhr, textStatus, errorThrown) {
+                    console.log("An error occurred when trying to obtain the nodes\n\njqXhr>" + jqXhr + "\n\ntextStatus>" + textStatus + "\n\nerrorThrown>" + errorThrown);
+                }
+            });
         },
         getChildrenNodesSuccess: function (itemId, parentElement, data) {
             if (data != null && data.length > 0) {
                 var childTreeTag = $("<ul />").addClass("treeview").appendTo(parentElement);
 
-                this.createNode(data, childTreeTag);
+                this.createNodeArray(data, childTreeTag);
 
                 this.bindCheckboxes();
 
@@ -45,15 +48,136 @@ var Treeview = (function () {
             }
         },
 
+        getAllNodes: function () {
+            var baseDataToSend = { };
+            var dataToSend = $.extend(true, baseDataToSend, this.options.dataToSend);
+            dataToSend = dataToSend == null ? "" : this.convertToString(dataToSend);
+
+            var mySelf = this;
+            $.ajax({
+                type: this.options.ajaxVerb,
+                contentType: "application/json; charset=utf-8",
+                url: this.options.sourceUrl,
+                data: dataToSend,
+                dataType: "json",
+                success: function (data) {
+                    mySelf.getAllNodesSuccess(data, mySelf.myObject);
+                },
+                error: function (jqXhr, textStatus, errorThrown) {
+                    console.log("An error occurred when trying to obtain the nodes\n\njqXhr>" + jqXhr + "\n\ntextStatus>" + textStatus + "\n\nerrorThrown>" + errorThrown);
+                }
+            });
+        },
+        getAllNodesSuccess: function (treeNode, parentElement) {
+            if (treeNode != null) {
+                var childTreeTag = $("<ul />").addClass("treeview").appendTo(parentElement);
+
+                if ($.isArray(treeNode)) {
+                    this.createNodeArray(treeNode, childTreeTag);
+                } else {
+                    this.createNode(treeNode, childTreeTag, "hit-single");
+                }
+
+                this.bindCheckboxes();
+            }
+        },
+        createNodeArray: function (dataSource, parentElement) {
+            for (var i = 0; i < dataSource.length; i++) {
+                var item = dataSource[i];
+                var hitPosition;
+
+                if (dataSource.length == 1) {
+                    hitPosition = "hit-single";
+                } else {
+                    switch (i) {
+                        case 0:
+                            hitPosition = "hit-first";
+                            break;
+                        case dataSource.length - 1:
+                            hitPosition = "hit-last";
+                            break;
+                        default:
+                            hitPosition = "";
+                            break;
+                    }
+                }
+
+                this.createNode(item, parentElement, hitPosition);
+            }
+        },
+        createNode: function (item, parentElement, hitPosition) {
+            // Create the base node li tag
+            var nodeInnerTag = $("<li />");
+            nodeInnerTag.attr("id", item.Id);
+            nodeInnerTag.data("item", item);
+            nodeInnerTag.addClass("node-bg-vimage");
+
+            // Create the span hit area (where the plus and minus sign appears)
+            var nodeSpanHit = $("<span />");
+            nodeSpanHit.attr("id", item.Id);
+            nodeSpanHit.addClass(hitPosition);
+            nodeSpanHit.appendTo(nodeInnerTag);
+
+
+            if (item.HasChild) {
+                nodeInnerTag.addClass("with-child-node");
+
+                nodeSpanHit.addClass("scontainer-expand");
+                nodeSpanHit.addClass("hit-area");
+
+                var mySelf = this;
+                nodeSpanHit.click(function () {
+                    mySelf.expandNode($(this).attr("id"));
+                });
+            } else {
+                nodeInnerTag.addClass("without-child-node");
+
+                nodeSpanHit.addClass("no-hit-area");
+            }
+
+            if (hitPosition == "hit-last" || hitPosition == "hit-single") {
+                nodeInnerTag.removeClass("node-bg-vimage");
+            }
+
+
+            // Create a span to be used as a render node container.
+            // The content of this container could be overridden by a custom _renderItem method.
+            var spanText = $("<span />");
+            spanText.addClass("scontainer-node-render-container");
+            spanText.appendTo(nodeInnerTag);
+
+
+            this.renderItem(spanText, item);
+
+
+            // Attaching the new node to the parent element
+            nodeInnerTag.appendTo(parentElement);
+
+            if (this.options.onNodeCreated)
+                this.options.onNodeCreated(nodeInnerTag, parentElement, this.myObject);
+
+            // Expand child nodes
+            if (item.HasChild && !this.options.loadNodesOnDemand) {
+                this.switchCssClass(nodeInnerTag.children(".scontainer-expand"));
+
+                this.getAllNodesSuccess(item.Children, nodeInnerTag);
+            }
+        },
+
+
         expandNode: function (itemNodeId) {
             var currentNode = this.getCurrentNodeById(itemNodeId);
 
             this.switchCssClass(currentNode.children(".scontainer-expand"));
 
-            this.getChildrenNodes(itemNodeId, currentNode, $.proxy(this.getChildrenNodesSuccess, this));
+            if (this.options.loadNodesOnDemand) {
+                this.getChildrenNodes(itemNodeId, currentNode, $.proxy(this.getChildrenNodesSuccess, this));
+            } else {
+                this.getAllNodesSuccess(currentNode.data("item").Children, currentNode);
+            }
 
             if (this.options.onNodeExpanded)
-                this.options.onNodeExpanded(this.myObject);
+                this.options.onNodeExpanded(currentNode, this.myObject);
         },
         collapseNode: function (itemNodeId) {
             var currentNode = this.getCurrentNodeById(itemNodeId);
@@ -63,7 +187,7 @@ var Treeview = (function () {
             currentNode.children("ul").remove();
 
             if (this.options.onNodeColapsed)
-                this.options.onNodeColapsed(this.myObject);
+                this.options.onNodeColapsed(currentNode, this.myObject);
         },
 
         renderItem: function (spanText, item) {
@@ -101,73 +225,6 @@ var Treeview = (function () {
                 }
             }
         },
-        createNode: function (dataSource, parentElement) {
-            for (var i = 0; i < dataSource.length; i++) {
-                var item = dataSource[i];
-                var hitPosition;
-
-                if (dataSource.length == 1) {
-                    hitPosition = "hit-single";
-                } else {
-                    switch (i) {
-                        case 0:
-                            hitPosition = "hit-first";
-                            break;
-                        case dataSource.length - 1:
-                            hitPosition = "hit-last";
-                            break;
-                        default:
-                            hitPosition = "";
-                            break;
-                    }
-                }
-
-                // Create the base node li tag
-                var nodeInnerTag = $("<li />");
-                nodeInnerTag.attr("id", item.Id);
-                nodeInnerTag.addClass("node-bg-vimage");
-                nodeInnerTag.addClass("without-child-node");
-
-                if (item.HasChild) {
-                    nodeInnerTag.removeClass("without-child-node").addClass("with-child-node");
-                }
-
-                if (hitPosition == "hit-last" || hitPosition == "hit-single") {
-                    nodeInnerTag.removeClass("node-bg-vimage");
-                }
-
-                // Create the span hit area (where the plus and minus sign appears)
-                var nodeSpanHit = $("<span />");
-                nodeSpanHit.attr("id", item.Id);
-                nodeSpanHit.addClass("no-hit-area");
-
-                if (item.HasChild) {
-                    nodeSpanHit.removeClass("no-hit-area").addClass("scontainer-expand").addClass("hit-area");
-
-                    var mySelf = this;
-                    nodeSpanHit.click(function () {
-                        mySelf.expandNode($(this).attr("id"));
-                    });
-                }
-
-                nodeSpanHit.addClass(hitPosition);
-                nodeSpanHit.appendTo(nodeInnerTag);
-
-                // Create a span to be used as a render node container.
-                // The content of this container could be overridden by a custom _renderItem method.
-                var spanText = $("<span />");
-                spanText.addClass("scontainer-node-render-container");
-                spanText.data("treeview-render-container-item", item);
-                this.renderItem(spanText, item);
-                spanText.appendTo(nodeInnerTag);
-
-                // Attaching the new node to the parent element
-                nodeInnerTag.appendTo(parentElement);
-
-                if (this.options.onNodeCreated)
-                    this.options.onNodeCreated(nodeInnerTag, parentElement, this.myObject);
-            }
-        },
         convertToString: function (obj) {
             if (typeof JSON != "undefined") {
                 return JSON.stringify(obj);
@@ -185,6 +242,7 @@ var Treeview = (function () {
         getCurrentNodeById: function (nodeId) {
             return this.myObject.find("li[id='" + nodeId + "']").first();
         },
+
         bindCheckboxes: function () {
             var checkboxes = this.myObject.find("input[type='checkbox']");
             checkboxes.off("change");
@@ -216,9 +274,9 @@ var Treeview = (function () {
                 this.getCurrentNodeCheckbox(currentContainer.parents("li")).prop({ checked: false });
             }
         },
-        getCurrentNodeCheckbox : function(element) {
+        getCurrentNodeCheckbox: function (element) {
             return element.children(".scontainer-node-render-container").children('input[type="checkbox"]');
-        },
+        }
     };
 
     return treeview;
@@ -237,7 +295,8 @@ var Treeview = (function () {
             renderItem: null,
             getChildrenNodes: null,
             showCheckbox: false,
-            checkboxesName: null
+            checkboxesName: null,
+            loadNodesOnDemand: true
         };
 
         var settings = $.extend({}, defaults, options);
