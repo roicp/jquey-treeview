247079d6f953746dd940992a4aace73e1a5b74b9
Rodrigo Ipaves de Campos Pinto <ipaves@gmail.com>
2020-11-10 11:28:50 -0300

    clean start

--- jquery-treeview.js
+++ jquery-treeview.js
@@ -1,17 +1,5 @@
 var Treeview = function () {
     'use strict';
 
-    var _privateProperty = 'Hello World';
 
-    function _privateMethod() {
-        console.log(_privateProperty);
-    }
-
-    function publicMethod() {
-        _privateMethod();
-    }
-
-    return {
-        publicMethod: publicMethod
-    };
 };
